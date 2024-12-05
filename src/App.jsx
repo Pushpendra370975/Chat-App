9c7d3344cf1f5de7cf0aeac03e16241ce45c5aca
Pushpendra Kumar <139001888+741737@users.noreply.github.com>
2024-12-05 16:52:50 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useState } from 'react' // Add useState
-import Chat from './components/chat/Chat'
-import List from './components/list/List'
-import Detail from './components/details/Detail'
-import Login from './components/login/Login'
-import Notification from './components/notification/Notification'
-import { auth } from './lib/firebase';
-import { onAuthStateChanged } from 'firebase/auth'
-import { useUserStore } from './lib/userStore'
-import { useChatStore } from './lib/chatStore'
+import React, { useEffect, useState } from "react"; // Add useState
+import Chat from "./components/chat/Chat";
+import List from "./components/list/List";
+import Detail from "./components/details/Detail";
+import Login from "./components/login/Login";
+import Notification from "./components/notification/Notification";
+import { auth } from "./lib/firebase";
+import { onAuthStateChanged } from "firebase/auth";
+import { useUserStore } from "./lib/userStore";
+import { useChatStore } from "./lib/chatStore";
 
 const App = () => {
   const [showDetail, setShowDetail] = useState(true); // Add this state
-  const {chatId} = useChatStore();
-  const {currentUser, isLoading, fetchUserInfo} = useUserStore();
-  
+  const { chatId } = useChatStore();
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
+
   useEffect(() => {
     console.log("Setting up auth listener");
     const unSub = onAuthStateChanged(auth, (user) => {
@@ -26,32 +26,30 @@ const App = () => {
     });
     return () => unSub();
   }, []);
-  
-  
+
   if (isLoading) {
     console.log("Showing loading state");
-    return <div className='loading'>Loading....</div>;
+    return <div className="loading">Loading....</div>;
   }
 
-  
   return (
-    <div className='container'>
+    <div className="container">
       {currentUser ? (
         <>
-          <List/>
+          <List />
           {chatId && (
-            <div className='chat-container'>
+            <div className="chat-container">
               <Chat showDetail={showDetail} setShowDetail={setShowDetail} />
-              {showDetail && <Detail/>}
+              {showDetail && <Detail />}
             </div>
           )}
         </>
       ) : (
-        <Login/>
+        <Login />
       )}
-      <Notification/>
+      <Notification />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
